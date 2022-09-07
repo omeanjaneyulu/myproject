cb45647da77fef48daf6685b252d3c98912392f2
omean <omean@yoshi>
2022-09-07 11:49:56 +0530

    string program

---
+++ java project/src/demo/Pattren.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Pattren {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str ="JAVACODINGREEL";
+for(int i=0;i<str.length();i++) {
+	for(int j=0;j<=i;j++) {
+		System.out.print(str.charAt(j));
+	}
+	System.out.println();
+	}
+}
+	}
+
+
