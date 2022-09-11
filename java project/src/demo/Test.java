bab86dbaaa4dfb3716bc2c546be833e4845a1a76
omean <omean@yoshi>
2022-09-11 15:24:55 +0530

    using string

---
+++ java project/src/demo/Test.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1 = "manu";
+String emailid = "manu26@gmail.com";
+System.out.println(s1);
+System.out.println(emailid);
+	}
+
+}
