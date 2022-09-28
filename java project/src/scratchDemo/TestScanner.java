98a3914a6f5578008d7189ec9a1185c0de03066e
omean <omean@yoshi>
2022-09-28 16:46:09 +0530

    Scanner example

---
+++ java project/src/scratchDemo/TestScanner.java
@@ -0,0 +1,22 @@
+package scratchDemo;
+
+import java.util.Scanner;
+
+public class TestScanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    /* Scanner sc=new Scanner(System.in);
+     System.out.println("please enter the name");
+     String name=sc.nextLine();
+     System.out.println("entered name is:"+name); 
+     */
+		
+		Scanner sc=new Scanner(System.in);
+	     System.out.println("please enter the number");
+	     int num=sc.nextInt();
+	     System.out.println("entered number is:"+num);
+
+	}
+
+}
